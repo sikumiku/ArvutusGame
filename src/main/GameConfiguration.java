1ecfcbe3b3946fc8b2ec018d8b078d08aee30783
Sigrid <sigridaasma@gmail.com>
2016-01-10 11:30:57 +0200

    Taimer resetitud 60'le

--- src/main/GameConfiguration.java
+++ src/main/GameConfiguration.java
@@ -5,14 +5,14 @@ package main;
  * Seadistab kogu mängu.
  */
 public class GameConfiguration {
-    public static final int TIMERSTARTCOUNT = 31; //Mitu sekundit kestab üks level.
+    public static final int TIMERSTARTCOUNT = 61; //Mitu sekundit kestab üks level.
 
     //Mis on kõige madalam väärtus, mida iga leveli puhul võidakse kuvada.
     public static final int TASE1_LOWEST_NUMBER = 1;
     public static final int TASE2_LOWEST_NUMBER = 3;
     public static final int TASE3_LOWEST_NUMBER = 6;
 
-    //Mis on kõige kõrgem väärtus, mida iga leveli puhul võidakse kuvada.
+    //Mis on kõige kõrgem väärtus, mida iga leveli puhul võidakse kuvada (LOW NUMBER + antud number).
     public static final int TASE1_HIGHEST_NUMBER = 6;
     public static final int TASE2_HIGHEST_NUMBER = 6;
     public static final int TASE3_HIGHEST_NUMBER = 6;
